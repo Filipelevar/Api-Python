7236dee9b872ed72642fef8391626c834a531717
Filipelevar <filipelevar@icloud.com>
2023-07-17 20:20:50 -0300

    Feature: i made a pagination and filter query

--- README.md
+++ README.md
@@ -8,7 +8,17 @@ Steps taken
 4 - config the api to connect with database PostgreSQL
 5 - Connect SQLAlchemy to my App
 6 - Create a Table Characters
-7 - create a class to insert items into characters
-8 - utilizando o metodo **init** e atribui os parametros para meu objeto characters
+7 - create a class to insert items into character
+8 - utilizando o metodo **init** e atribui os parametros para meu objeto character
 9 - dentro da minha rota primaria, eu faço meu JSON ser lido, os dados necessários são extraídos e usados ​​para criar um novo Characters objeto.
 10 - criei um For para adicionar os dados do JSON lido no meu banco de dados como um Looping
+11 - Importar o Request e Marshmallow
+
+Após os dados inseridos proximo passo foi criar
+
+1 - Criei as instancias (ma) para utilizar o Marshmallow e coloquei como parametro ele dentro do APP
+2 - Criei uma class CharacterSchema para definir o objeto Character para ser serializados/desserializados com marshmallow
+3 - Criei os parametros de consulta e defini a paginação utilizando OffSet e filtrei as paginas de busca por nome
+4 - Apos isso retorna os dados em JSON
+
+acessar a pesquisa de personagem no banco de dados (http://localhost:105/characters?page=1&per_page=20&name=nome)
--- app.py
+++ app.py
@@ -1,12 +1,60 @@
-from flask import Flask
-
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:filipe1020@localhost:5432/RickandMorty'
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
 
 
-@app.route('/home/', methods=['GET', 'POST'])
-def welcome():
-    return "Hello World!"
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    status = db.Column(db.String(200))
+    species = db.Column(db.String(200))
+    type = db.Column(db.String(200), nullable=True)
+    gender = db.Column(db.String(200))
+    origin_name = db.Column(db.String(200))
+    location_name = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+    def __init__(self, name, status, species, type, gender, origin_name, location_name, image):
+        self.name = name
+        self.status = status
+        self.species = species
+        self.type = type
+        self.gender = gender
+        self.origin_name = origin_name
+        self.location_name = location_name
+        self.image = image
+
+
+class CharacterSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Character
+
+
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    page = int(request.args.get('page', 1))
+    name_filter = request.args.get('name', '')
+
+    limit = 20
+
+    query = Character.query.filter(Character.name.ilike(f'{name_filter}%'))
+    characters = query.limit(limit).offset((page - 1) * limit).all()
+
+    character_schema = CharacterSchema(many=True)
+    result = character_schema.dump(characters)
+
+    return jsonify({
+        'characters': result,
+        'total_pages': len(characters) // limit + 1,
+        'current_page': page,
+        'total_items': len(characters)
+    })
 
 
 if __name__ == '__main__':
